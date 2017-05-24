96e88637e784be558092a6607a16cfd08a6f68ab
pjaraba <pe.jaraba@hotmail.com>
2017-05-24 10:13:50 -0500

    se cambio principal para que cogiera

--- src/main/java/edu/uac/spark/servidor/Principal.kt
+++ src/main/java/edu/uac/spark/servidor/Principal.kt
@@ -5,12 +5,11 @@ import edu.uac.negocio.casos_de_uso.ListadorDeUsuarios
 import edu.uac.spark.controladores.ControladorPrueba
 import spark.Spark.*
 
-/**
- * Created by pjaraba on 5/22/2017.
- */
 
 fun main(args: Array<String>){
-    port(8080)
+    val puertoEnv = System.getenv("PORT")
+    val puerto: Int = if(puertoEnv == null) 8080 else Integer.getInteger(puertoEnv)
+    port(puerto)
     val repositorioUsuarios = RepositorioUsuariosPrueba()
     val listadorDeUsuarios = ListadorDeUsuarios(repositorioUsuarios)
     ControladorPrueba(listadorDeUsuarios)
